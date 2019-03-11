e9cbd7d3eaea62a86fb69b1aad85b6cfb8209837
Danielle Jasper <dnj547@live.com>
2019-03-11 19:40:55 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,11 @@
-# Write your code here.
+def word_substituter(tweet)
+end
+
+def bulk_tweet_shortener(tweets)
+end
+
+def selective_tweet_shortener(tweet)
+end
+
+def shortened_tweet_truncator(tweet)
+end
