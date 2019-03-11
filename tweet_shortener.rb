9175378bba8b26a6e1ea967254f8d7b54215371a
Danielle Jasper <dnj547@live.com>
2019-03-11 19:46:31 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,4 +1,16 @@
+def dictionary
+  dictionary_hash = {
+    "hi" => "hello",
+    "2" => ["to", "two", "too"],
+    "4" => ["for", "four"],
+    "b" => "be",
+    "@" => "at",
+    "&" => "and"
+  }
+end
+
 def word_substituter(tweet)
+  tweet
 end
 
 def bulk_tweet_shortener(tweets)
