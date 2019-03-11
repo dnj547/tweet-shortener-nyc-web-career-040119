23ec6e67fd34d7cba9d3b35f81d1954ea3c8943f
Danielle Jasper <dnj547@live.com>
2019-03-11 20:04:18 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -25,7 +25,7 @@ def word_substituter(tweet)
 end
 
 def bulk_tweet_shortener(tweets)
-  tweets.each {|tweet| word_substituter(tweet)}
+  tweets.each {|tweet| puts word_substituter(tweet)}
 end
 
 def selective_tweet_shortener(tweet)
